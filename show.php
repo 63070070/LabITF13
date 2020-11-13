9eb2c602d1bf61a45e68d4f91f3afcb039dd5fa1
63070070 <73686792+63070070@users.noreply.github.com>
2020-11-13 15:05:18 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'labitf-13.mysql.database.azure.com', '63070070@labitf-13', 'MESjmr18', 'itflab', 3306);
+mysqli_real_connect($conn, 'labitf-13.mysql.database.azure.com', 'it63070070@labitf-13', 'MESjmr18', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
