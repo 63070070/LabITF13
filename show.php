f6b0edf7ca86e0d9cb89265749534b4dc2ab460b
63070070 <73686792+63070070@users.noreply.github.com>
2020-11-13 15:28:09 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,8 +1,8 @@
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<title>ITF Lab</title>
 </head>
+<title>ITF Lab</title>
 <body>
 <?php
 $conn = mysqli_init();
