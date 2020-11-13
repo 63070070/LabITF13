60e46b3878b848b3e87873df72568e464d4ece18
63070070 <73686792+63070070@users.noreply.github.com>
2020-11-13 16:24:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,8 +29,8 @@ while($Result = mysqli_fetch_array($res))
     <td class="text-success"><?php echo $Result['Name'];?></div></td>
     <td class="text-success"><?php echo $Result['Comment'];?></td>
     <td class="text-success"><?php echo $Result['Link'];?></td>
-    <td><input type="แก้ไข" id="commentBtn" class="btn btn-success" align="center">
-        <input type="ลบ" id="commentBtn" class="btn btn-success" align="center"></td>
+    <td><input type="submit" id="commentBtn" class="btn btn-success" align="center" text="แก้ไข>
+        <input type="submit" id="commentBtn" class="btn btn-success" align="center"></td>
   </tr>
 <?php
 }
