db2ce5bfdd5a4cb146ee46e7ba330f60a1349af4
63070070 <73686792+63070070@users.noreply.github.com>
2020-11-14 14:51:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ while($Result = mysqli_fetch_array($res))
     <td class="text-success"><?php echo $Result['Comment'];?></td>
     <td class="text-success"><?php echo $Result['Link'];?></td>
     <td><button type="button" class="btn btn-outline-warning" onclick="location.href='form.html'">แก้ไข</button>
-        <button type="button" class="btn btn-outline-danger" onclick="location.href="delete.php"">ลบ</button>
+        <button type="button" class="btn btn-outline-danger" onclick="location.href="delete.php?ID=<?php echo $Result["ID"]; ?>"">ลบ</button>
         <input type="button" class="btn btn-outline-danger" value="ลบ" value="<?php echo $Result["ID"]; ?>" onclick="delete.php">
     </td>
   </tr>
