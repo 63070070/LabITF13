5ebf2bfc66ca5b22d48c0953f998262c47aa5787
63070070 <73686792+63070070@users.noreply.github.com>
2020-11-14 18:12:28 +0700

    Update form_edit.php

--- form_edit.php
+++ form_edit.php
@@ -1,7 +1,10 @@
 <?php
-    	$id=$_GET['ID'];
-	$conn = mysqli_init();
+$id=$_GET['ID'];
+$conn = mysqli_init();
 mysqli_real_connect($conn, 'labitf-13.mysql.database.azure.com', 'it63070070@labitf-13', 'MESjmr18', 'itflab', 3306);
+$res = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID='$id'");
+$row = mysqli_fetch_array($res)
+
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
@@ -17,14 +20,14 @@ if (mysqli_connect_errno($conn))
 </head>
 <body>
 
-  <form action = "edit.php?ID=<?php echo $id?>" method = "post" id="CommentForm" >
+  <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm" class="form-group">
     Name:<br>
-    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    <input type="text" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter comment"></textarea><br> 
+    <input type="text" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
     Link:<br>
-    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
-    <input type="submit" id="commentBtn" class="btn btn-warning" value="Update">
+    <input type="text" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
+    <input type="submit" id="commentBtn"class="btn btn-outline-warning">
   </form> 
 
 </body>
